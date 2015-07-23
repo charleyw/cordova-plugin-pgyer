414edd8fb9f94c06fcbd4826bd738fcc1cdbf450
Chao Wang <cwang8023@gmail.com>
2015-07-23 14:42:42 +0800

    add auto init for android

--- src/android/PgyerPlugin.java
+++ src/android/PgyerPlugin.java
@@ -8,42 +8,37 @@ import android.util.Log;
 
 import com.pgyersdk.update.PgyUpdateManager;
 
-import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.json.JSONArray;
-import org.json.JSONException;
+import org.apache.cordova.CordovaWebView;
 
 public class PgyerPlugin extends CordovaPlugin {
     private static String TAG = "PgyerPlugin";
 
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("init")) {
-            String androidAppID = "";
-            try {
-                ApplicationInfo applicationInfo = cordova.getActivity().getPackageManager().getApplicationInfo(cordova.getActivity().getPackageName(), PackageManager.GET_META_DATA);
-                Bundle bundle = applicationInfo.metaData;
-                androidAppID = bundle.getString("PGYER_APP_ID");
-            } catch (NameNotFoundException e) {
-                Log.e(TAG, "Failed to load meta-data, NameNotFound: " + e.getMessage());
-            } catch (NullPointerException e) {
-                Log.e(TAG, "Failed to load meta-data, NullPointer: " + e.getMessage());
-            }
-
-            if(androidAppID.equals("")){
-                return true;
-            }
-
-            final String appID = androidAppID;
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    PgyUpdateManager.register(cordova.getActivity(), appID);
-                    Log.i(TAG, "Pgyer update check registered, app ID: " + appID);
-                }
-            });
-            return true;
-        } else {
-            return false;
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        String androidAppID = "";
+        try {
+            ApplicationInfo applicationInfo = cordova.getActivity().getPackageManager().getApplicationInfo(cordova.getActivity().getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = applicationInfo.metaData;
+            androidAppID = bundle.getString("PGYER_APP_ID");
+        } catch (NameNotFoundException e) {
+            Log.e(TAG, "Failed to load meta-data, NameNotFound: " + e.getMessage());
+        } catch (NullPointerException e) {
+            Log.e(TAG, "Failed to load meta-data, NullPointer: " + e.getMessage());
         }
+
+        if(androidAppID.equals("")){
+            return;
+        }
+
+        final String appID = androidAppID;
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                PgyUpdateManager.register(cordova.getActivity(), appID);
+                Log.i(TAG, "Pgyer update check registered, app ID: " + appID);
+            }
+        });
     }
 }
